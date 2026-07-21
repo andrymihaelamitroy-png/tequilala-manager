6953b60fc3c5a8b34f73bec935346a82818eb2e3
andrymihaelamitroy-png <andrymihaelamitroy@gmail.com>
2026-07-21 22:28:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const client = new Client({
   ]
 });
 
+// =============================
+// CONFIGURACIÓN
+// =============================
+
+const GUILD_ID = '1311766874542837871';
+
+const POSTULACIONES_CHANNEL_ID = '1529194473072889896';
+const POSTULACIONES_CATEGORY_ID = '1529220895846174730';
+const REGISTRO_POSTULACIONES_CHANNEL_ID = '1529221287262818584';
+
+const ENCARGADOS_ROLE_ID = '1529222035950014625';
+const GERENCIA_ROLE_ID = '1311766874568261670';
+
 client.once('clientReady', () => {
   console.log(`Bot conectado como ${client.user.tag}`);
 });
